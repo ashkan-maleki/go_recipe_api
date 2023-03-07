133e6e05105dac867e7277f8ba32ce2a4577b759
mamalmaleki <mohammadalimalekie@gmail.com>
2023-03-07 21:24:57 +0330

    recipe data model

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func main() {
 	router := gin.Default()
 	router.Run()
+}
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
