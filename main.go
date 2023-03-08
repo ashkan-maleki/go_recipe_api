08c640dc23be301c641ac2673523a7cded8305e2
mamalmaleki <mohammadalimalekie@gmail.com>
2023-03-08 18:26:44 +0330

    delete recipe endpoint

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func main() {
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipeHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
+	router.DELETE("/recipes/:id", DeleteRecipeHandler)
 	router.Run()
 }
 
@@ -86,4 +87,23 @@ func UpdateRecipeHandler(c *gin.Context) {
 	recipe.ID = id
 	recipes[index] = recipe
 	c.JSON(http.StatusOK, recipe)
+}
+
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	index := -1
+
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Recipe not found"})
+		return
+	}
+
+	recipes = append(recipes[:index], recipes[index+1:]...)
+	c.JSON(http.StatusOK, gin.H{"message": "Recipe has been deleted"})
 }
